fb1f785607920c213b3f2863137614a4cf3b2268
Jake Bolton <ninmonkeys@gmail.com>
2023-05-22 15:58:25 -0500

    new: Validate IsIntOrString

---
+++ KnowMoreTangent/Data/Vault/ScriptBlock.Template/ValidationAttributes/Validate.IsIntOrString.ps1
@@ -0,0 +1,24 @@
+# thread started at <https://discord.com/channels/180528040881815552/447476117629304853/1110201488472539296>
+# About: Creating a validator to allow integers or string, nothing else.
+class ValidateStringOrInt : System.Management.Automation.ValidateEnumeratedArgumentsAttribute {
+    [Object]$Value
+    [void]ValidateElement( $Element ) {
+        $ex = [Management.Automation.MetadataException]
+
+        if ($null -eq $Element) {
+            throw $ex::new( 'Value cannot be true null null!' )
+        }
+        if (-not( $Element -is 'string' -or $Element -is 'int'  ) ) {
+            throw  $ex::new(
+                "Value '$Element' is not an [int] or [string]. Was a [$($Element.GetType().Name)]")
+        }
+    }
+}
+<#
+    Remove-Variable @('a'..'z') -ea 'ignore'
+    [ValidateStringOrInt()]$a = '123'
+    [ValidateStringOrInt()]$b = 123
+    [ValidateStringOrInt()]$c = '123.3'
+    { [ValidateStringOrInt()]$e = 23.435 } | Should -Throw
+    Get-Variable @('a'..'z') -ea 'ignore' | ft
+#>
---
+++ KnowMoreTangent/Data/Vault/ScriptBlock.Template/ValidationAttributes/Validate.IsIntOrString.tests.ps1
@@ -0,0 +1,90 @@
+<#
+ New settings in
+    5.2: <https://pester.dev/docs/commands/New-PesterConfiguration>
+        old config: <https://pester.dev/docs/usage/configuration>
+    5.3: <https://pester.dev/docs/usage/output#stacktraceverbosity>
+    5.4: <https://pester.dev/docs/usage/output#rendermode>
+
+Stack Verbosity
+    None, FirstLine, Filtered (default), Full
+
+CIFormat
+
+    None, Auto (default), AzureDevops, GithubActions
+
+RenderMode
+    Auto (default), ANSI, ConsoleColor, PlainText
+
+
+#>
+
+
+$pref = $PesterPreference = [PesterConfiguration]::Default
+$PesterPreference.Debug.WriteDebugMessages = $false
+$PesterPreference.Output.Verbosity = 'None'
+
+BeforeAll {
+    $PSStyle.OutputRendering = 'ansi'
+
+    # share the same class name, stripping revisions off.
+    # $Src = $PSCommandPath -replace '(-v\d+)\.tests\.ps1', '.ps1'
+    # or just stripping suffix
+    $Src = $PSCommandPath -replace '.tests\.ps1', '.ps1'
+    . (Get-Item -ea stop $Src)
+    '::imported' | Write-Host -fore green
+}
+
+Context 'Attribute [ValidatingIntOrString]' {
+    Describe '<Value> as <ExpectType>' -ForEach @(
+        @{
+            Exp         = { [ValidateStringOrInt()]$SomeInt = 123; $SomeInt }
+            ExpectType  = 'int'
+            Value       = 123
+            ShouldThrow = $false
+        }
+        @{
+            Exp         = { [ValidateStringOrInt()]$someKind = 123 ; $someKind }
+            ExpectType  = 'int'
+            Value       = 123
+            ShouldThrow = $false
+        }
+        @{
+            Exp         = { [ValidateStringOrInt()]$someKind = '123' ; $someKind }
+            ExpectType  = 'string'  #  can I auto-assert as int
+            Value       = '123'
+            ShouldThrow = $false
+        }
+        @{
+            Exp         = { [ValidateStringOrInt()]$someKind = '123.4' ; $someKind }
+            ExpectType  = 'string'
+            Value       = '123.4'
+            ShouldThrow = $false
+        }
+        @{
+            Exp         = { [ValidateStringOrInt()]$someKind = $null ; $someKind }
+            ShouldThrow = $true
+        }
+        @{
+            Exp         = { [ValidateStringOrInt()]$someKind = ''; $someKind }
+            ExpectType  = 'string'
+            Value       = ''
+            ShouldThrow = $false # unless I want blanks. for now allow it.
+        }
+    ) {
+        It 'Is equal to <Value>' {
+            & $Exp | Should -Be $Value
+        }
+
+        It 'If <Value> ShouldThrow <ShouldThrow> and did' {
+            $Exp | Should -Throw -Not:$(-not $ShouldThrow)
+        }
+        It 'If ShouldThrow <ShouldThrow> and <Value> isType <ExpectType>' {
+            if ($ShouldThrow) {
+                Set-ItResult -Skipped -Because 'WouldHaveThrown, ie: no value.'
+                return # appears to ignore later Should statements, so, maybe redundant?
+            }
+            & $Exp | Should -BeOfType $ExpectType
+        }
+
+    }
+}
